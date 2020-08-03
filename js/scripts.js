8b84ed444edaf120bfa811e145080dd289fc2498
Grace <wanenegrace@gmail.com>
2020-08-03 07:24:42 +0300

    Adding JS to new branch

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,30 @@ function Pizza(size, crust){
   this.toppings = [];
 }
 
+Pizza.prototype.fullSizeCrust = function() {
+  return this.size + " " + this.crust;
+}
+
 $(document).ready(function(){
-  $(".selectSize").click(function() {
+  $("#selectOrder").click(function() {
   	event.preventDefault();
   var value = $(this).val();
   alert(value);
 });
+});
+
+$(document).ready(function() {
+  $("form#buildPizza").clickfunction(event) {
+    event.preventDefault();
+
+    var inputtedSize = $("#selectSize").val();
+    var inputtedCrust = $("#selectCrust").val();
+
+    var newPizza = new Pizza(inputtedSize, inputtedCrust);
+
+    $("ul#orderList").append("<li><span class='orderItem'>" + newPizza.fullSizeCrust + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
 });
