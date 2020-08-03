156c88963dc8226df9b7de8aea64a79df179c749
Grace <wanenegrace@gmail.com>
2020-08-03 06:17:57 +0300

    trying new inout form in newform branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
-function Pizza(toppings, size){
+function Pizza(size, crust){
   this.size = size;
   this.crust = crust;
   this.toppings = [];
 }
+
+$(document).ready(function(){
+  $(".selectSize").click(function() {
+  	event.preventDefault();
+  var value = $(this).val();
+  alert(value);
+});
+});
