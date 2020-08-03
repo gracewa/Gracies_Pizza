8201cefee2a4adc60a095f86b2265f13a1cbfa4e
Grace <wanenegrace@gmail.com>
2020-08-03 08:39:49 +0300

    Modified form and Added js

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,40 @@
-function Pizza(size, crust){
-  this.size = size;
-  this.crust = crust;
-  this.toppings = [];
-}
+function Pizza(size, crust, toppings) {
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
+    this.pizzaPrice = 0;
+  }
+  Pizza.prototype.totalCost = function() {
+    if (this.size === "500") {
+      this.pizzaPrice += parseInt(this.size);
+    } else if (this.size === "750") {
+      this.pizzaPrice += parseInt(this.size);
+    } else if (this.size === "1000") {
+      this.pizzaPrice += parseInt(this.size);
+    }
+    if (this.crust === "crispy") {
+      this.pizzaPrice += 100;
+    } else if (this.crust === "stuffed") {
+      this.pizzaPrice += 200;
+    } else if (this.crust === "glutenFree") {
+      this.pizzaPrice += 300;
+    }
+    if (this.toppings === "pepperoni") {
+      this.pizzaPrice += 100;
+    } else if (this.toppings === "beef") {
+      this.pizzaPrice += 100;
+    } else if (this.toppings === "chicken") {
+      this.pizzaPrice += 100;
+    } else if (this.toppings === "vegeterian") {
+      this.pizzaPrice += 100;
+    }
+  };
 
-Pizza.prototype.fullSizeCrust = function() {
-  return this.size + " " + this.crust;
-}
+  $(document).ready(function(){
+      $("form").submit(function(){
+          $(".total").show();
 
-$(document).ready(function(){
-  $("#selectOrder").click(function() {
-  	event.preventDefault();
-  var value = $(this).val();
-  alert(value);
-});
-});
+          event.preventDefault();
 
-$(document).ready(function() {
-  $("form#buildPizza").clickfunction(event) {
-    event.preventDefault();
-
-    var inputtedSize = $("#selectSize").val();
-    var inputtedCrust = $("#selectCrust").val();
-
-    var newPizza = new Pizza(inputtedSize, inputtedCrust);
-
-    $("ul#orderList").append("<li><span class='orderItem'>" + newPizza.fullSizeCrust + "</span></li>");
-
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
+      });
   });
-});
